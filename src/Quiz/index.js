f6ead55937ea9aad2cbfb5e8c92cd96632954bb7
Chandra Shekhar <shekhar@arista.com>
2023-12-22 22:15:15 +0530

    meal fid

--- src/Quiz/index.js
+++ src/Quiz/index.js
@@ -1,28 +1,50 @@
-import { getQuestions, random } from "./db.js"
-import { useState, useEffect } from "react";
+import { getQuestions, random } from './db.js';
+import { useState } from 'react';
+import './Quiz.css';
 
 const questionBase = getQuestions()();
 
 function QuizQuestion() {
-    const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState(0);
 
-    const qusNumber = questionBase.map((k) => {
-        return { ...k, isSelected: true }
-    })
-    console.log()
-    useEffect(() => {
-        setQuestions(qusNumber)
-    }, [])
+  const handelNextQuestion = () => {
+    setQuestions(questions + 1);
+  };
 
+  console.log(questionBase);
 
-
-
-    return (
-        <div>{questions.map((k) => <div >
-            <div>  Question{k.id}</div>
-            <div> { } </div>
-
-        </div>)}</div>
-    )
+  return (
+    <div className="quiz-container">
+      <div className="heading">
+        <h2>Quiz Application UI</h2>
+      </div>
+      <div className="display-content">
+        <div className="content-container">
+          <div className="Quiz-title">Quiz Title</div>
+          <div className="questionNo ">
+            <div> {questions + 1}.</div>
+            <div>{questionBase[questions].question}</div>
+          </div>
+          <div className="Option-content">
+            {questionBase[questions].options.map((k, i) => {
+              return <div className="Ques-btn">{k.text}</div>;
+            })}
+          </div>
+          <div onClick={handelNextQuestion}>next</div>
+        </div>
+        <div className="option-bar">
+          <div className="option-heading">
+            <div>Question</div>
+            <div className="need-help">Need Help?</div>
+          </div>
+          <div>
+            {questionBase.map((k, i) => (
+              <div className="count">{i}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default QuizQuestion;
